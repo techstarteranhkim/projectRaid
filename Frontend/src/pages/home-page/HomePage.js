364475786475821cb98fad6d0b1beb01cae7fa05
techstarteranhkim <anh.kim@docc.techstarter.de>
2024-05-07 10:00:02 +0200

    implemented winstonlogger for homepage

--- Frontend/src/pages/home-page/HomePage.js
+++ Frontend/src/pages/home-page/HomePage.js
@@ -2,8 +2,10 @@ import styles from "./HomePage.module.css";
 import TranslateInput from "../../components/translation-components/translate-input";
 import TranslateOutput from "../../components/translation-components/translate-output";
 import SearchHistory from "../../components/translation-components/search-history";
+import Logger from "winston";
 
 function HomePage() {
+  Logger.info("Homepage loaded successfully");
   return (
     <div className={styles.mainContainer}>
       <div className={styles.translationContainer}>
